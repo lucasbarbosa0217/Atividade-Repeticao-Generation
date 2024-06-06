9a1f4c98b9ddcbfdeaf0e8a193634053a4f6a756
Lucas Barbosa <lucasbarbosa0217@gmail.com>
2024-06-06 15:33:30 -0300

    Terminei ultimas atividades

---
+++ src/repeticao/AtividadeFor2.java
@@ -0,0 +1,31 @@
+package repeticao;
+
+import java.util.Scanner;
+
+public class AtividadeFor2 {
+	public static void main(String[] args) {
+		/*
+		 * Escreva um algoritmo em Java, que leia 2 números inteiros via teclado, onde o
+		 * primeiro número deve ser menor do que o segundo número. Caso contrário, deve
+		 * ser exibida uma mensagem na tela informando que o intervalo é inválido e sair
+		 * do programa. No intervalo informado, mostre na tela todes os números que são
+		 * múltiplos de 3 e 5. Veja os exemplos abaixo:
+		 */
+
+		Scanner scanner = new Scanner(System.in);
+		int numero, pares = 0, impares = 0;
+
+		for (int i = 0; i < 10; i++) {
+			System.out.printf("Digite o %dº número: ", i + 1);
+			numero = scanner.nextInt();
+
+			if (numero % 2 == 0) {
+				pares++;
+			} else {
+				impares++;
+			}
+		}
+		System.out.printf("Total de números pares: %d", pares);
+		System.out.printf("%nTotal de números ímpares: %d", impares);
+	}
+}
---
+++ src/repeticao/AtividadeWhile2.java
@@ -0,0 +1,41 @@
+package repeticao;
+
+import java.util.Scanner;
+
+public class AtividadeWhile2 {
+	public static void main(String[] args) {
+
+		/*
+		 * Escreva um algoritmo em Java, que leia a idade de várias pessoas (números
+		 * inteiros), via teclado e mostre na tela o total de pessoas cuja idade seja
+		 * menor que 21 anos e o total de pessoas cuja idade seja maior que 50 anos. A
+		 * leitura dos dados deve ser finalizada ao digitar uma idade negativa. Veja o
+		 * exemplo abaixo
+		 */
+
+		Scanner scanner = new Scanner(System.in);
+		int idade = 0, anos21 = 0, anos50 = 0;
+		float media;
+
+		while(idade >= 0) {
+			System.out.print("Digite uma idade: ");
+			idade = scanner.nextInt();
+
+			if (idade <= 21 && idade >= 0) {
+				anos21++;
+			}
+			
+			if(idade >= 50) {
+				anos50++;
+			}
+
+		} 
+
+		scanner.close();
+
+		System.out.printf("Total de pessoas menores de 21 anos: %d",anos21);
+		System.out.printf("\nTotal de pessoas maiores de 50 anos: %d",anos50);
+
+	}
+
+}
